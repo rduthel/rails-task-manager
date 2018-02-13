654b021b4a12cc0035493e7cce1c68615409bc1e
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-13 15:21:35 +0100

    can add a task, via a form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,32 @@
 class TasksController < ApplicationController
+  before_action :set_task, :completed, only: :show
+
   def index
     @tasks = Task.all
   end
 
-  def show
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
     @task = Task.find(params[:id])
+  end
+
+  def completed
+    @task_completed = @task.completed ? 'Completed' : 'Not completed'
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
