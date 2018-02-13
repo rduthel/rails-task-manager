09d87a6a0c4fb0320399716c8c12ce0ff1132c13
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-13 12:07:15 +0100

    dropped and generate db to init completed with false value

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180213101516) do
+ActiveRecord::Schema.define(version: 20180213110219) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
