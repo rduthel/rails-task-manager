e02816d41eb734e9a47ffaadea1443b7272ee9ef
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-13 13:47:27 +0100

    added a show method to display details of a task

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'index', to: 'tasks#index', as: :index
+  get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks/:id', to: 'tasks#show', as: :task
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
